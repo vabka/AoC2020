17450351de60d45cdfa36d382d60a0160ec948d1
Vasiliy Bannikov <vab-999@ya.ru>
2020-12-05 19:10:06 +0500

    Самый хитрый способ конвертации билетов

--- src/tasks/5/b.fsx
+++ src/tasks/5/b.fsx
@@ -1,61 +1,24 @@
 open System.IO
 open System
 open System.Collections.Immutable
+open System.Text.RegularExpressions
 
-type Range(Min: int, Max: int) =
-    let length = Max - Min + 1
-    member _.LowerHalf = Range(Min, Max - length / 2)
-    member _.UpperHalf = Range(Min + length / 2, Max)
-    member _.Exact = if length = 1 then Some(Max) else None
-
-type Direction =
-    | Front
-    | Back
-    | Left
-    | Right
-
-let getDirection l =
-    match l with
-    | 'F' -> Some(Front)
-    | 'B' -> Some(Back)
-    | 'L' -> Some(Left)
-    | 'R' -> Some(Right)
-    | _ -> None
-
-
-let isSome (x: 'a option) = x.IsSome
-let getVal (x: 'a option) = x.Value
-
-
-let seatId row column = 8 * row + column
-let getSeatIdFromTicket str =
-    let folder (rowRange: Range, columnRange: Range) (d: Direction) =
-        match d with
-        | Front -> (rowRange.LowerHalf, columnRange)
-        | Back -> (rowRange.UpperHalf, columnRange)
-        | Left -> (rowRange, columnRange.LowerHalf)
-        | Right -> (rowRange, columnRange.UpperHalf)
-
-    let (r, c) =
-        str
-        |> Seq.map getDirection
-        |> Seq.filter isSome
-        |> Seq.map getVal
-        |> Seq.fold folder (Range(0, 127), Range(0, 7))
-    match (r.Exact, c.Exact) with
-    | Some (r), Some (c) -> seatId r c
-    | _ -> raise <| InvalidOperationException()
+let getSeatIdFromTicket (str: string) =
+    let binStr =        
+        let rg1 = Regex("R|B")
+        let rg0 = Regex(@"\D")
+        let ones = rg1.Replace(str, "1")
+        rg0.Replace(ones, "0")
+    Convert.ToInt32(binStr, 2)
 
 let puzzleInput = File.ReadAllLines "./input.txt"
 
-let myList = puzzleInput |> Seq.map getSeatIdFromTicket |> Seq.toList
+let allTickets = puzzleInput |> Seq.map getSeatIdFromTicket |> Seq.toList
 
-let min = myList |> List.min
-let max = myList |> List.max
-
+let min = allTickets |> List.min
+let max = allTickets |> List.max
 let fullSum = [min..max] |> List.sum
-let actualSum = myList |> List.sum
-
+let actualSum = allTickets |> List.sum
 let result = fullSum - actualSum
 
 printfn "%i" result
