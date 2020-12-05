cc730686d803082caaccad2fd5ce80b4055e3e6a
Vasiliy Bannikov <vab-999@ya.ru>
2020-12-05 20:00:56 +0500

    форматируем

--- src/tasks/5/a.fsx
+++ src/tasks/5/a.fsx
@@ -1,8 +1,20 @@
 open System.IO
 open System
+
 let getSeatIdFromTicket (ticket: string) =
-    let binStr = (((ticket.Replace('R', '1')).Replace('B', '1')).Replace('L', '0')).Replace('F', '0')
-    Convert.ToInt32(binStr, 2)
+    let replace (n: char) (o: char) (str: string) = str.Replace(o, n)
+    let toZero = replace '1'
+    let toOne = replace '0'
+
+    let bits =
+        ticket
+        |> toOne 'R'
+        |> toZero 'L'
+        |> toOne 'B'
+        |> toZero 'F'
+
+    Convert.ToInt32(bits, 2)
+
 File.ReadAllLines "./input.txt"
 |> Seq.map getSeatIdFromTicket
 |> Seq.max
--- src/tasks/5/b.fsx
+++ src/tasks/5/b.fsx
@@ -2,17 +2,32 @@ open System.IO
 open System
 
 let getSeatIdFromTicket (ticket: string) =
-    let binStr = (((ticket.Replace('R', '1')).Replace('B', '1')).Replace('L', '0')).Replace('F', '0')
-    Convert.ToInt32(binStr, 2)
+    let replace (n: char) (o: char) (str: string) = str.Replace(o, n)
+    let toZero = replace '1'
+    let toOne = replace '0'
+
+    let bits =
+        ticket
+        |> toOne 'R'
+        |> toZero 'L'
+        |> toOne 'B'
+        |> toZero 'F'
+
+    Convert.ToInt32(bits, 2)
 
 let puzzleInput = File.ReadAllLines "./input.txt"
 
-let allTickets = puzzleInput |> Seq.map getSeatIdFromTicket |> Seq.toList
+let allTickets =
+    puzzleInput
+    |> Seq.map getSeatIdFromTicket
+    |> Seq.toList
 
 let min = allTickets |> List.min
 let max = allTickets |> List.max
-let fullSum = [min..max] |> List.sum
-let actualSum = allTickets |> List.sum
-let result = fullSum - actualSum
 
-printfn "%i" result
+let fullSum = [ min .. max ] |> List.sum
+let actualSum = allTickets |> List.sum
+
+let ourSeat = fullSum - actualSum
+
+printfn "%i" ourSeat
