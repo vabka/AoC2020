b757c27794d71a8b2c14fb5a4385d32b01df0fe2
Vasiliy Bannikov <vab-999@ya.ru>
2020-12-05 19:20:43 +0500

    избавляемся от регулярок

--- src/tasks/5/b.fsx
+++ src/tasks/5/b.fsx
@@ -1,14 +1,8 @@
 open System.IO
 open System
-open System.Collections.Immutable
-open System.Text.RegularExpressions
 
-let getSeatIdFromTicket (str: string) =
-    let binStr =        
-        let rg1 = Regex("R|B")
-        let rg0 = Regex(@"\D")
-        let ones = rg1.Replace(str, "1")
-        rg0.Replace(ones, "0")
+let getSeatIdFromTicket (ticket: string) =
+    let binStr = (((ticket.Replace('R', '1')).Replace('B', '1')).Replace('L', '0')).Replace('F', '0')
     Convert.ToInt32(binStr, 2)
 
 let puzzleInput = File.ReadAllLines "./input.txt"
