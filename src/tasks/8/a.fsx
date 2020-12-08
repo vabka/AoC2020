7ae7444fb80345a57d035897c3c3cfc7ede2b6f0
Vabka <vab-999@ya.ru>
2020-12-08 09:43:11 +0500

    Готовимся к 8 дню

---
+++ src/tasks/8/a.fsx
@@ -0,0 +1,10 @@
+#load "../common.fsx"
+
+open Common
+open System
+open System.Collections.Generic
+open System.IO
+
+let input = File.ReadAllText "input.txt"
+
+
---
+++ src/tasks/common.fsx
@@ -0,0 +1,24 @@
+open System.Text.RegularExpressions
+open System.Collections.Generic
+open System
+
+let matches rg text =
+  let matches = Regex.Matches(rg, text)
+
+  seq {
+    for m in matches do
+      let d = Dictionary()
+
+      for group in m.Groups do
+        d.Add(group.Name, group.Value)
+
+      yield d :> IReadOnlyDictionary<string, string>
+  }
+
+let split (separator: string) (text: string) =
+  text.Split([| separator |], StringSplitOptions.RemoveEmptyEntries)
+
+let splitByLine =
+  split (Environment.NewLine + Environment.NewLine)
+
+let splitByEndl = split Environment.NewLine
